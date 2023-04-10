ba23a0f0efffde6bfa0b91d41bd1466d90243c57
ckw <3238573552@qq.com>
2023-04-10 23:13:34 +0800

    修正了增加菜品信息时的bug，新增前台点赞功能

--- apps/crud.py
+++ apps/crud.py
@@ -1,5 +1,5 @@
 from operator import attrgetter
-
+import datetime
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from .models import *
@@ -202,7 +202,8 @@ def add_dish(db: Session, dish_message: DishMessage):
         "level": dish_message.level,
         "window": dish_message.window,
         "price": dish_message.price,
-        "size": dish_message.size
+        "size": dish_message.size,
+        "likes": 0
     }
     dish = Dishes(**dish_dict)
     db.add(dish)
@@ -349,3 +350,68 @@ def get_canteens_message(db: Session, canteens: list):
     if not canteens_information:
         raise HTTPException(status_code=404, detail="获取失败，无更多信息")
     return canteens_information
+
+
+# 前台
+def get_dish_message(db: Session, dishes: list):
+    dishes_information = []
+    for dish in dishes:
+        window = get_window_by_window_id(db, dish.window_id)
+        level = get_level_by_level_id(db, window.level_id)
+        canteen = get_canteen_by_canteen_id(db, level.canteen_id)
+        campus = get_campus_by_id(db, canteen.campus_id)
+        dish_information = {
+            "name": dish.dish_name,
+            "price": dish.price,
+            "position": campus.campus_name,
+            "image": dish.photos,
+            "labels": [""]
+        }
+        dishes_information.append(dish_information)
+    return dishes_information
+
+
+# def random_draw(db: Session, canteen_name: str, time: str):
+#     listx = get_dishes_by_name_and_time(db, canteen_name, time)
+#     lx = len(listx)
+#     r = random.randint(0, lx)
+#     return listx[r]
+
+
+# def get_canteen_by_name(db: Session, canteen_name: str):
+#     return db.query(Canteens).filter(Canteens.canteen_name == canteen_name).first()
+
+
+def get_dishes_by_canteen_and_level(db: Session, canteen_id: str, level: int):
+    # canteen = get_canteen_by_name(db, canteen_name)
+    # canteen_id = canteen.canteen_id
+    return db.query(Dishes).filter(Dishes.canteen_id == canteen_id, Dishes.level == level).all()
+
+
+def get_dishes_by_name_and_time(db: Session, canteen_id: str, timex: str):
+    # canteen = get_canteen_by_name(db, canteen_name)
+    # canteen_id = canteen.canteen_id
+    if timex == "早":
+        return db.query(Dishes).filter(Dishes.canteen_id == canteen_id, Dishes.morning == 1).all()
+    elif timex == "中":
+        return db.query(Dishes).filter(Dishes.canteen_id == canteen_id, Dishes.noon == 1).all()
+    else:
+        return db.query(Dishes).filter(Dishes.canteen_id == canteen_id, Dishes.night == 1).all()
+
+
+def get_like(db: Session, openid: int, dish_id: str):
+    return db.query(Like).filter(Like.openid == openid, Like.dish_id == dish_id).first()
+
+
+def add_like(db: Session, openid: int, dish_id: str):
+    date_ = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    like_dict = {
+        "openid": openid,
+        "dish_id": dish_id,
+        "date_": date_
+    }
+
+    like = Like(**like_dict)
+    db.add(like)
+    db.commit()
+    db.refresh(like)
--- apps/frontpage.py
+++ apps/frontpage.py
@@ -1,9 +1,76 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from .models import *
+from .schemas import *
+from .crud import *
+import datetime
 from .background import *
 
 frontpage = APIRouter()
 
 
-@frontpage.get("/")
-async def star():
-    return "Hello,World"
+# 交互文档登录
 
+@frontpage.get("/dishes", status_code=200, response_description="got successfully", summary="获取菜品信息")
+async def get_this_dishes(db: Session = Depends(get_db), level: int = 1, canteen_id: str = '0'):
+    dishes = get_dishes_by_canteen_and_level(db, canteen_id, level)
+    dishes_information = get_dish_message(db, dishes)
+    return {"message": "success", "detail": "获取成功", "data": {"dishes_information": dishes_information}}
+
+
+# @frontpage.get("/draw_dish", status_code=200, response_description="got successfully", summary="随机抽取菜品")
+# async def draw_dish(db: Session = Depends(get_db), canteen_id: str = '0', timex: str = '0'):
+#
+#     return random_draw(db, canteen_id, timex)
+
+@frontpage.get("/draw_dishes", status_code=200, response_description="got successfully", summary="随机抽取菜品")
+async def draw_dish(db: Session = Depends(get_db), canteen_id: str = '01', timex: str = '0'):
+    dishes = get_dishes_by_name_and_time(db, canteen_id, timex)
+    dishes_information = get_dish_message(db, dishes)
+    return {"message": "success", "detail": "获取成功", "data": {"dishes_information": dishes_information}}
+
+
+@frontpage.get("/likes", status_code=200, response_description="got successfully", summary="查询点赞")
+async def get_like_status(dish_id: str, openid: int, db: Session = Depends(get_db)):
+    like = get_like(db, openid, dish_id)
+    dish = get_dish_by_dish_id(db, dish_id)
+    if not like:
+        return {"message": "success", "detail": "获取成功", "data": {"like_information": {
+            "like": False,
+            "time": None,
+            "like_num": dish.likes
+        }}}
+    if like:
+        return {"message": "success", "detail": "获取成功", "data": {"like_information": {
+            "like": True,
+            "time": like.date_,
+            "like_num": dish.likes
+        }}}
+
+
+@frontpage.put("/likes", status_code=200, response_description="changed successfully", summary="点赞或取消")
+async def chang_like_status(dish_id: str, openid: int, db: Session = Depends(get_db)):
+    like = get_like(db, openid, dish_id)
+    dish = get_dish_by_dish_id(db, dish_id)
+    if not like:
+        add_like(db, openid, dish_id)
+        new_like = get_like(db, openid, dish_id)
+        dish.likes = dish.likes + 1
+        db.commit()
+        db.refresh(dish)
+        return {"message": "success", "detail": "点赞成功", "data": {"like_information": {
+            "like": True,
+            "time": new_like.date_,
+            "like_num": dish.likes
+        }}}
+    if like:
+        dish.likes = dish.likes - 1
+        db.commit()
+        db.refresh(dish)
+        db.delete(like)
+        db.commit()
+        return {"message": "success", "detail": "取消点赞成功", "data": {"like_information": {
+            "like": False,
+            "time": None,
+            "like_num": dish.likes
+        }}}
